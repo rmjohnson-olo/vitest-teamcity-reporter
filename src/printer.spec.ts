55ebc218bcc7f0050e3eaee1fafecec4913d4f90
Ryan Johnson <ryan.johnson@olo.com>
2022-10-12 14:06:11 -0500

    Use flowId to prevent TC from being confused by parallel running tests.

--- src/printer.spec.ts
+++ src/printer.spec.ts
@@ -1,202 +1,209 @@
-import { describe, it, vi, expect, File, Test, Suite, TaskResultPack } from 'vitest'
-import { print, printTask, printTaskResultPack } from './printer'
+import { describe, it, vi, expect, File, Test, Suite, TaskResultPack } from 'vitest';
+import { print, printTask, printTaskResultPack } from './printer';
 
-vi.stubGlobal('console', { info: vi.fn() })
+vi.stubGlobal('console', { info: vi.fn() });
 
 describe('printer', () => {
-  describe('print', () => {
-    it('should print given message and args to console.info', () => {
-      //given
-      const message = 'test message'
-      const args = ['some', 'args']
+    describe('print', () => {
+        it('should print given message and args to console.info', () => {
+            //given
+            const message = 'test message';
+            const args = ['some', 'args'];
 
-      //when
-      print(message, ...args)
+            //when
+            print(message, ...args);
 
-      //then
-      expect(console.info).toBeCalledWith(message, ...args)
-    })
-  })
+            //then
+            expect(console.info).toBeCalledWith(message, ...args);
+        });
+    });
 
-  describe('printTask', () => {
-    it('should print suite and nested test', () => {
-      //given
-      const suite: File = {
-        type: 'suite',
-        mode: 'run',
-        id: 'some-id',
-        name: 'some-suite',
-        tasks: [],
-        filepath: '/some/path'
-      }
-      const nestedSuite: Suite = {
-        type: 'suite',
-        mode: 'run',
-        id: 'some-nested-suite',
-        name: 'some-nested-suite',
-        tasks: [],
-        suite: suite
-      }
-      suite.tasks.push(nestedSuite)
-      const nestedTest: Test = {
-        type: 'test',
-        mode: 'run',
-        id: 'some-test',
-        name: 'some-test',
-        suite: nestedSuite,
-        context: new (vi.fn())
-      }
-      nestedSuite.tasks.push(nestedTest)
-      const taskIndex = new Map()
+    describe('printTask', () => {
+        it('should print suite and nested test', () => {
+            //given
+            const suite: File = {
+                type: 'suite',
+                mode: 'run',
+                id: 'some-id',
+                name: 'some-suite',
+                tasks: [],
+                filepath: '/some/path',
+            };
+            const nestedSuite: Suite = {
+                type: 'suite',
+                mode: 'run',
+                id: 'some-nested-suite',
+                name: 'some-nested-suite',
+                tasks: [],
+                suite: suite,
+            };
+            suite.tasks.push(nestedSuite);
+            const nestedTest: Test = {
+                type: 'test',
+                mode: 'run',
+                id: 'some-test',
+                name: 'some-test',
+                suite: nestedSuite,
+                context: new (vi.fn())(),
+            };
+            nestedSuite.tasks.push(nestedTest);
+            const taskIndex = new Map();
 
-      //when
-      printTask(taskIndex)(suite)
+            //when
+            printTask(taskIndex)(suite);
 
-      //then
-      expect(console.info).toHaveBeenCalledWith(`##teamcity[testSuiteStarted name='${suite.name}']`)
-      expect(console.info).toHaveBeenCalledWith(`##teamcity[testSuiteStarted name='${nestedSuite.name}']`)
-      expect(console.info).toHaveBeenCalledWith(`##teamcity[testStarted name='${nestedTest.name}']`)
-      expect(taskIndex).toContain(suite)
-      expect(taskIndex).toContain(nestedSuite)
-      expect(taskIndex).toContain(nestedTest)
-    })
+            //then
+            expect(console.info).toHaveBeenCalledWith(`##teamcity[testSuiteStarted name='${suite.name}' flowId='${suite.id}']`);
+            expect(console.info).toHaveBeenCalledWith(`##teamcity[testSuiteStarted name='${nestedSuite.name}' flowId='${nestedSuite.id}']`);
+            expect(console.info).toHaveBeenCalledWith(`##teamcity[testStarted name='${nestedTest.name}' flowId='${nestedTest.id}']`);
+            expect(taskIndex).toContain(suite);
+            expect(taskIndex).toContain(nestedSuite);
+            expect(taskIndex).toContain(nestedTest);
+        });
 
-    it("should print skipped tests", () => {
-      //given
-      const suite: File = {
-        type: 'suite',
-        mode: 'run',
-        id: 'some-id',
-        name: 'some-suite',
-        tasks: [],
-        filepath: '/some/path'
-      }
-      const nestedSuite: Suite = {
-        type: 'suite',
-        mode: 'skip',
-        id: 'some-nested-suite',
-        name: 'some-nested-suite',
-        tasks: [],
-        suite: suite
-      }
-      suite.tasks.push(nestedSuite)
-      const nestedTest: Test = {
-        type: 'test',
-        mode: 'skip',
-        id: 'some-test',
-        name: 'some-test',
-        suite: nestedSuite,
-        context: new (vi.fn())
-      }
-      nestedSuite.tasks.push(nestedTest)
-      const taskIndex = new Map()
+        it('should print skipped tests', () => {
+            //given
+            const suite: File = {
+                type: 'suite',
+                mode: 'run',
+                id: 'some-id',
+                name: 'some-suite',
+                tasks: [],
+                filepath: '/some/path',
+            };
+            const nestedSuite: Suite = {
+                type: 'suite',
+                mode: 'skip',
+                id: 'some-nested-suite',
+                name: 'some-nested-suite',
+                tasks: [],
+                suite: suite,
+            };
+            suite.tasks.push(nestedSuite);
+            const nestedTest: Test = {
+                type: 'test',
+                mode: 'skip',
+                id: 'some-test',
+                name: 'some-test',
+                suite: nestedSuite,
+                context: new (vi.fn())(),
+            };
+            nestedSuite.tasks.push(nestedTest);
+            const taskIndex = new Map();
 
-      //when
-      printTask(taskIndex)(suite)
+            //when
+            printTask(taskIndex)(suite);
 
-      //then
-      expect(console.info).toHaveBeenCalledWith(`##teamcity[testSuiteStarted name='${suite.name}']`)
-      expect(console.info).not.toHaveBeenCalledWith(`##teamcity[testSuiteStarted name='${nestedSuite.name}']`)
-      expect(console.info).toHaveBeenCalledWith(`##teamcity[testIgnored name='${nestedTest.name}' message='']`)
-      expect(taskIndex).toContain(suite)
-      expect(taskIndex).toContain(nestedSuite)
-      expect(taskIndex).toContain(nestedTest)
-    })
-  })
+            //then
+            expect(console.info).toHaveBeenCalledWith(`##teamcity[testSuiteStarted name='${suite.name}' flowId='${suite.id}']`);
+            expect(console.info).not.toHaveBeenCalledWith(
+                `##teamcity[testSuiteStarted name='${nestedSuite.name}' flowId='${nestedSuite.id}']`
+            );
+            expect(console.info).toHaveBeenCalledWith(
+                `##teamcity[testIgnored name='${nestedTest.name}' message='' flowId='${nestedTest.id}']`
+            );
+            expect(taskIndex).toContain(suite);
+            expect(taskIndex).toContain(nestedSuite);
+            expect(taskIndex).toContain(nestedTest);
+        });
+    });
 
-  describe('printTaskResultPack', () => {
-    it("should print passt suite as finished", () => {
-      //given
-      const taskResult: TaskResultPack[1] = {
-        state: 'pass',
-      }
-      const suite: Suite = {
-        type: 'suite',
-        mode: 'skip',
-        id: 'some-nested-suite',
-        name: 'some-nested-suite',
-        tasks: [],
-        suite: new (vi.fn())
-      }
-      const taskIndex = new Map()
-      taskIndex.set('id', suite)
+    describe('printTaskResultPack', () => {
+        it('should print passt suite as finished', () => {
+            //given
+            const taskResult: TaskResultPack[1] = {
+                state: 'pass',
+            };
+            const suite: Suite = {
+                type: 'suite',
+                mode: 'skip',
+                id: 'some-nested-suite',
+                name: 'some-nested-suite',
+                tasks: [],
+                suite: new (vi.fn())(),
+            };
+            const taskIndex = new Map();
+            taskIndex.set('id', suite);
 
-      //when
-      printTaskResultPack(taskIndex)(['id', taskResult])
+            //when
+            printTaskResultPack(taskIndex)(['id', taskResult]);
 
-      //then
-      expect(console.info).toHaveBeenCalledWith(`##teamcity[testSuiteFinished name='${suite.name}']`)
-    })
+            //then
+            expect(console.info).toHaveBeenCalledWith(`##teamcity[testSuiteFinished name='${suite.name}' flowId='${suite.id}']`);
+        });
 
-    it('shoult print passt test as finished', () => {
-      //given
-      const taskResult: TaskResultPack[1] = {
-        state: 'pass',
-        duration: 10,
-      }
-      const test: Test = {
-        type: 'test',
-        mode: 'run',
-        id: 'some-test',
-        name: 'some-test',
-        suite: new (vi.fn()),
-        context: new (vi.fn())
-      }
-      const taskIndex = new Map()
-      taskIndex.set('id', test)
+        it('shoult print passt test as finished', () => {
+            //given
+            const taskResult: TaskResultPack[1] = {
+                state: 'pass',
+                duration: 10,
+            };
+            const test: Test = {
+                type: 'test',
+                mode: 'run',
+                id: 'some-test',
+                name: 'some-test',
+                suite: new (vi.fn())(),
+                context: new (vi.fn())(),
+            };
+            const taskIndex = new Map();
+            taskIndex.set('id', test);
 
-      //when
-      printTaskResultPack(taskIndex)(['id', taskResult])
+            //when
+            printTaskResultPack(taskIndex)(['id', taskResult]);
 
-      //then
-      expect(console.info).toHaveBeenCalledWith(`##teamcity[testFinished name='${test.name}' duration='${taskResult.duration}']`)
-    })
+            //then
+            expect(console.info).toHaveBeenCalledWith(
+                `##teamcity[testFinished name='${test.name}' duration='${taskResult.duration}' flowId='${test.id}']`
+            );
+        });
 
-    it("should print skipped test as ignored", () => {
-      //given
-      const taskResult: TaskResultPack[1] = {
-        state: 'skip',
-      }
-      const test: Test = {
-        type: 'test',
-        mode: 'run',
-        id: 'some-test',
-        name: 'some-test',
-        suite: new (vi.fn()),
-        context: new (vi.fn())
-      }
-      const taskIndex = new Map()
-      taskIndex.set('id', test)
+        it('should print skipped test as ignored', () => {
+            //given
+            const taskResult: TaskResultPack[1] = {
+                state: 'skip',
+            };
+            const test: Test = {
+                type: 'test',
+                mode: 'run',
+                id: 'some-test',
+                name: 'some-test',
+                suite: new (vi.fn())(),
+                context: new (vi.fn())(),
+            };
+            const taskIndex = new Map();
+            taskIndex.set('id', test);
 
-      //when
-      printTaskResultPack(taskIndex)(['id', taskResult])
+            //when
+            printTaskResultPack(taskIndex)(['id', taskResult]);
 
-      //then
-      expect(console.info).toHaveBeenCalledWith(`##teamcity[testIgnored name='${test.name}' message='']`)
-    })
+            //then
+            expect(console.info).toHaveBeenCalledWith(`##teamcity[testIgnored name='${test.name}' message='' flowId='${test.id}']`);
+        });
 
-    it("should print failed suite as failed", () => {
-      //given
-      const taskResult: TaskResultPack[1] = {
-        state: 'fail',
-      }
-      const test: Test = {
-        type: 'test',
-        mode: 'run',
-        id: 'some-test',
-        name: 'some-test',
-        suite: new (vi.fn()),
-        context: new (vi.fn())
-      }
-      const taskIndex = new Map()
-      taskIndex.set('id', test)
+        it('should print failed suite as failed', () => {
+            //given
+            const taskResult: TaskResultPack[1] = {
+                state: 'fail',
+            };
+            const test: Test = {
+                type: 'test',
+                mode: 'run',
+                id: 'some-test',
+                name: 'some-test',
+                suite: new (vi.fn())(),
+                context: new (vi.fn())(),
+            };
+            const taskIndex = new Map();
+            taskIndex.set('id', test);
 
-      //when
-      printTaskResultPack(taskIndex)(['id', taskResult])
+            //when
+            printTaskResultPack(taskIndex)(['id', taskResult]);
 
-      //then
-      expect(console.info).toHaveBeenCalledWith(`##teamcity[testFailed name='${test.name}' message='' details='' actual='' expected='' type='comparisonFailure']`)
-    })
-  })
-})
-
+            //then
+            expect(console.info).toHaveBeenCalledWith(
+                `##teamcity[testFailed name='${test.name}' message='' details='' actual='' expected='' type='comparisonFailure' flowId='${test.id}']`
+            );
+        });
+    });
+});
--- src/printer.ts
+++ src/printer.ts
@@ -1,89 +1,95 @@
-
 import { Benchmark, Task, TaskResultPack, Test } from 'vitest';
 // eslint-disable-next-line no-console
 const print = (message: unknown, ...args: string[]) => console.info(message, ...args);
 
 // See https://www.jetbrains.com/help/teamcity/service-messages.html#Supported+Test+ServiceMessages
-const printSuiteStarted = (name: string) => print(`##teamcity[testSuiteStarted name='${name}']`);
-const printTestStarted = (name: string) => print(`##teamcity[testStarted name='${name}']`);
-const printTestFailed = (name: string, { message = '', details = '', actual = '', expected = '' }) =>
-  print(
-    `##teamcity[testFailed name='${name}' message='${message}' details='${details}' actual='${actual}' expected='${expected}' type='comparisonFailure']`
-  );
-const printTestIgnored = (name: string, message = '') => print(`##teamcity[testIgnored name='${name}' message='${message}']`);
-const printTestFinished = (name: string, duration: number) => print(`##teamcity[testFinished name='${name}' duration='${duration}']`);
-const printSuiteFinished = (name: string) => print(`##teamcity[testSuiteFinished name='${name}']`);
+const printSuiteStarted = (name: string, flowId: string) => print(`##teamcity[testSuiteStarted name='${name}' flowId='${flowId}']`);
+const printTestStarted = (name: string, flowId: string) => print(`##teamcity[testStarted name='${name}' flowId='${flowId}']`);
+const printTestFailed = (name: string, { message = '', details = '', actual = '', expected = '' }, flowId: string) =>
+    print(
+        `##teamcity[testFailed name='${name}' message='${message}' details='${details}' actual='${actual}' expected='${expected}' type='comparisonFailure' flowId='${flowId}']`
+    );
+const printTestIgnored = (name: string, message = '', flowId: string) =>
+    print(`##teamcity[testIgnored name='${name}' message='${message}' flowId='${flowId}']`);
+const printTestFinished = (name: string, duration: number, flowId: string) =>
+    print(`##teamcity[testFinished name='${name}' duration='${duration}' flowId='${flowId}']`);
+const printSuiteFinished = (name: string, flowId: string) => print(`##teamcity[testSuiteFinished name='${name}' flowId='${flowId}']`);
 
 const escape = (str: string = ''): string => {
-  return str
-    .toString()
-    .replace(/\x1B.*?m/g, '')
-    .replace(/\|/g, '||')
-    .replace(/\n/g, '|n')
-    .replace(/\r/g, '|r')
-    .replace(/\[/g, '|[')
-    .replace(/\]/g, '|]')
-    .replace(/\u0085/g, '|x')
-    .replace(/\u2028/g, '|l')
-    .replace(/\u2029/g, '|p')
-    .replace(/'/g, "|'");
+    return str
+        .toString()
+        .replace(/\x1B.*?m/g, '')
+        .replace(/\|/g, '||')
+        .replace(/\n/g, '|n')
+        .replace(/\r/g, '|r')
+        .replace(/\[/g, '|[')
+        .replace(/\]/g, '|]')
+        .replace(/\u0085/g, '|x')
+        .replace(/\u2028/g, '|l')
+        .replace(/\u2029/g, '|p')
+        .replace(/'/g, "|'");
 };
 const buildTestName = (task: Test | Benchmark) => `${escape(task.name)}`;
 
-type TaskIndex = Map<string, Task>
+type TaskIndex = Map<string, Task>;
 
 const printTask = (taskIndex: TaskIndex) => (task: Task) => {
-  if (task.type === 'suite') {
-    if (task.mode === 'run') {
-      printSuiteStarted(task.name);
-    }
-    taskIndex.set(task.id, task);
-    task.tasks.forEach(printTask(taskIndex));
-  }
-  else if (task.type === 'test') {
-    const name = buildTestName(task);
-    if (task.mode === 'skip') {
-      printTestIgnored(name);
-    }
-    if (task.mode === 'run') {
-      printTestStarted(name);
-    }
-    taskIndex.set(task.id, task);
-  }
-};
-
-const printTaskResultPack = (taskIndex: TaskIndex) => ([id, result]: TaskResultPack) => {
-  if (taskIndex.has(id)) {
-    const task = taskIndex.get(id);
-    if (!task || !result) return;
-
     if (task.type === 'suite') {
-      const name = escape(task.name);
-      printSuiteFinished(name);
-    } else {
-      const name = buildTestName(task);
-      switch (result.state) {
-        case 'skip':
-          printTestIgnored(name);
-          break;
-        case 'pass':
-          printTestFinished(name, result.duration ?? 0);
-          break;
-        case 'fail':
-          printTestFailed(name, {
-            message: result.error?.message,
-            details: result.error?.stackStr,
-            actual: result.error?.actual,
-            expected: result.error?.expected,
-          });
-          break;
-        default:
-          // do nothing
-          break;
-      }
+        if (task.mode === 'run') {
+            printSuiteStarted(task.name, task.id);
+        }
+        taskIndex.set(task.id, task);
+        task.tasks.forEach(printTask(taskIndex));
+    } else if (task.type === 'test') {
+        const name = buildTestName(task);
+        if (task.mode === 'skip') {
+            printTestIgnored(name, undefined, task.id);
+        }
+        if (task.mode === 'run') {
+            printTestStarted(name, task.id);
+        }
+        taskIndex.set(task.id, task);
     }
-  }
 };
+
+const printTaskResultPack =
+    (taskIndex: TaskIndex) =>
+    ([id, result]: TaskResultPack) => {
+        if (taskIndex.has(id)) {
+            const task = taskIndex.get(id);
+            if (!task || !result) return;
+
+            if (task.type === 'suite') {
+                const name = escape(task.name);
+                printSuiteFinished(name, task.id);
+            } else {
+                const name = buildTestName(task);
+                switch (result.state) {
+                    case 'skip':
+                        printTestIgnored(name, undefined, task.id);
+                        break;
+                    case 'pass':
+                        printTestFinished(name, result.duration ?? 0, task.id);
+                        break;
+                    case 'fail':
+                        printTestFailed(
+                            name,
+                            {
+                                message: result.error?.message,
+                                details: result.error?.stackStr,
+                                actual: result.error?.actual,
+                                expected: result.error?.expected,
+                            },
+                            task.id
+                        );
+                        break;
+                    default:
+                        // do nothing
+                        break;
+                }
+            }
+        }
+    };
 
 export { printTask, printTaskResultPack, print };
 export type { TaskIndex };
